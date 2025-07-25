3e361247bd431e4f6597a41ac3f90b2fd948feee
esraaktts <f231213043@ktun.edu.tr>
2025-07-25 16:20:13 +0300

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import List, Optional
 
 class WordResponse(BaseModel):
-    word: str
+    word: Optional[str]
     phonetic: Optional[str] = None
     meanings: Optional[List[str]] = None
     title: Optional[str] = None
