414a1e24fc261dbea517a7d36372dd4d2c147bbd
esraaktts <f231213043@ktun.edu.tr>
2025-07-30 12:23:15 +0300

    #General fixes

--- word_of_the_day.py
+++ word_of_the_day.py
@@ -6,10 +6,8 @@ def word_of_the_day(word: str):
     try:
         r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
-        word_list= [ #ornek liste sadece test etmek icin anlam eklemedim
-            "resilient", "serendipity", "ephemeral", "ubiquitous", 
-            "mellifluous", "serene", "eloquent", "profound"
-        ]
+        with open('saved_words.json', 'r') as r:
+            word_list = json.load(r)
         word = r.get("word_of_the_day")
 
         if word:
@@ -20,8 +18,9 @@ def word_of_the_day(word: str):
             print("No word of the day found in Redis, generating a new one.")
             random_word = random.choice(word_list)
             new_word = {
-                "word": random_word,
-                "definition": f"This is a definition for the word '{random_word}'."
+                "word": random_word["word"],
+                "definition": random_word["meaning"],
+                "tags": random_word["tags"]
             }
 
         r.set("word_of_the_day", json.dumps(new_word),ex=86400)
